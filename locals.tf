e1931dfcf574c54536e946ebde61f92a25252b2f
Utku Sarioglu <utkusarioglu@hotmail.com>
2022-04-24 13:16:42 +0300

    Remove top-level local vars
    deleted:    locals.tf
    modified:   providers.tf
    - Remove local vars used in `backend` and `provider aws`. Terraform doesn't
      allow provider or backend params to come from variables.

--- locals.tf
+++
@@ -1,5 +0,0 @@
-locals {
-  # This is also defined in gh actions aws.yml
-  # If a change is required, change both
-  region = "eu-central-1"
-}
--- providers.tf
+++ providers.tf
@@ -8,12 +8,12 @@ terraform {
   backend "s3" {
     bucket         = "ws-aws-terraform-state"
     key            = "global/s3/terraform.tfstate"
-    region         = local.region
+    region         = "eu-central-1"
     dynamodb_table = "ws-aws-terraform-gh-actions-locks"
     encrypt        = true
   }
 }
 
 provider "aws" {
-  region = local.region
+  region = "eu-central-1"
 }
