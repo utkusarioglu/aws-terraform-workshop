20b9df1b3f4dc85c90de6e7170feb5988610d0dd
Utku Sarioglu <utkusarioglu@hotmail.com>
2022-04-25 13:23:41 +0300

    Remove state tools, these are now managed by the admin repo

--- state.tf
+++ state.tf
@@ -1,22 +1,22 @@
-resource "aws_s3_bucket" "ws_aws_tf_state" {
-  bucket = "ws-aws-terraform-state"
-}
+# resource "aws_s3_bucket" "ws_aws_tf_state" {
+#   bucket = "ws-aws-terraform-state"
+# }
 
-resource "aws_s3_bucket_versioning" "ws_aws_tf_state" {
-  bucket = aws_s3_bucket.ws_aws_tf_state.id
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
+# resource "aws_s3_bucket_versioning" "ws_aws_tf_state" {
+#   bucket = aws_s3_bucket.ws_aws_tf_state.id
+#   versioning_configuration {
+#     status = "Enabled"
+#   }
+# }
 
-resource "aws_s3_bucket_server_side_encryption_configuration" "ws_aws_tf_state" {
-  bucket = aws_s3_bucket.ws_aws_tf_state.bucket
-  rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
-    }
-  }
-}
+# resource "aws_s3_bucket_server_side_encryption_configuration" "ws_aws_tf_state" {
+#   bucket = aws_s3_bucket.ws_aws_tf_state.bucket
+#   rule {
+#     apply_server_side_encryption_by_default {
+#       sse_algorithm = "AES256"
+#     }
+#   }
+# }
 
 # resource "aws_dynamodb_table" "ws_aws_terraform_state_lock" {
 #   name         = "ws-aws-terraform-gh-actions-locks"
