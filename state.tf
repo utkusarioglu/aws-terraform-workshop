9496a1b82009e64901eb87fd816b82fba8c72873
Utku Sarioglu <utkusarioglu@hotmail.com>
2022-04-25 10:56:00 +0300

    Temporarily disable dynamodb resource
    state.tf
    - This is done for testing purposes

--- state.tf
+++ state.tf
@@ -18,12 +18,12 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "ws_aws_tf_state" 
   }
 }
 
-resource "aws_dynamodb_table" "ws_aws_terraform_state_lock" {
-  name         = "ws-aws-terraform-gh-actions-locks"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-}
+# resource "aws_dynamodb_table" "ws_aws_terraform_state_lock" {
+#   name         = "ws-aws-terraform-gh-actions-locks"
+#   billing_mode = "PAY_PER_REQUEST"
+#   hash_key     = "LockID"
+#   attribute {
+#     name = "LockID"
+#     type = "S"
+#   }
+# }
